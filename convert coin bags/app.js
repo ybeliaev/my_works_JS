3eda4c8f742f853cda4d576ce46a003e358dedea
Yura <ybeliaev@gmail.com>
2017-09-14 19:39:37 +0300

    formula to convert coin bags to their money amount.

---
+++ convert coin bags/app.js
@@ -0,0 +1,22 @@
+﻿let coins = {qs: 3, ds: 10, ns: 20, ps: 12} // числа монет каждого типа
+
+
+function coinsToAmount(obj) {
+  let sum = 0;
+  for(let prop in obj){
+    if(prop == 'qs'){
+      obj[prop] *= 0.25;
+    } else if (prop == 'ds'){
+      obj[prop] *= 0.1;
+    } else if (prop == 'ns'){
+      obj[prop] *= 0.05;
+    } else {
+      obj[prop] *= 0.01;
+    }
+      sum += obj[prop]
+    }
+    
+  return sum
+}
+
+console.log(coinsToAmount(coins)) // 1 ($)
