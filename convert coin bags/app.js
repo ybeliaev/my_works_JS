7084b1dde41910dfd5300ca7a0d42f57c26f12e6
Yura <ybeliaev@gmail.com>
2017-09-14 21:51:29 +0300

    Upp2

--- convert coin bags/app.js
+++ convert coin bags/app.js
@@ -1,22 +1,11 @@
-﻿let coins = {qs: 3, ds: 10, ns: 20, ps: 12} // числа монет каждого типа
+let coins = {qs: 3, ds: 10, ns: 20, ps: 12};// числа монет каждого типа
 
-
+let rate = {qs: 0.25, ds: 0.1, ns: 0.05, ps: 0.01};
 function coinsToAmount(obj) {
   let sum = 0;
-  for(let prop in obj){
-    if(prop == 'qs'){
-      obj[prop] *= 0.25;
-    } else if (prop == 'ds'){
-      obj[prop] *= 0.1;
-    } else if (prop == 'ns'){
-      obj[prop] *= 0.05;
-    } else {
-      obj[prop] *= 0.01;
-    }
-      sum += obj[prop]
-    }
-    
-  return sum
+  for (let prop in obj){
+    sum += obj[prop] * rate[prop];
+  } 
+    return sum;
 }
-
-console.log(coinsToAmount(coins)) 
+coinsToAmount(coins);
